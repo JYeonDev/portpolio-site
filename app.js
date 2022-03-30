836001b53753a2b926b6c0ab76e3015f6afbbcde
JYeonDev <whddus4837@gmail.com>
2022-03-30 18:11:51 +0900

    Add dark mode javascript

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// typingwrite
+
 const content = "반갑습니다, \n 성장하고 발전하며 \n 웹개발자를 목표로 하는 \n 김종연입니다.";
 const text = document.querySelector(".text");
 let i = 0;
@@ -11,3 +13,22 @@ function typing(){
     }
 }
 setInterval(typing, 200);
+
+// main-menu
+
+const light = document.querySelector('.light-mode');
+const dark = document.querySelector('.dark-mode');
+
+light.addEventListener('click', () => {
+  document.querySelectorAll('.main-menu-fixed li')[0].style.display = 'none'; 
+  document.querySelectorAll('.main-menu-fixed li')[1].style.display = 'block';
+  document.body.style.backgroundColor = 'black';
+  document.body.style.color = 'white';
+})
+
+dark.addEventListener('click', () => {
+  document.querySelectorAll('.main-menu-fixed li')[0].style.display = 'block'; 
+  document.querySelectorAll('.main-menu-fixed li')[1].style.display = 'none';
+  document.body.style.backgroundColor = 'white';
+  document.body.style.color = 'black';
+})
